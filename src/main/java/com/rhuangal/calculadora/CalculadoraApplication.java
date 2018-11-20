9793c8a5b1fb8df11c74f3ae075443c8c5c0232f
rhuangal <kirasoft@gmail.com>
2018-11-20 11:07:13 -0500

    Adding swagger.

--- src/main/java/com/rhuangal/calculadora/CalculadoraApplication.java
+++ src/main/java/com/rhuangal/calculadora/CalculadoraApplication.java
@@ -2,8 +2,10 @@ package com.rhuangal.calculadora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CalculadoraApplication {
 
 	public static void main(String[] args) {
